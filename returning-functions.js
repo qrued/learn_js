7da59a1f776ac0a354fdaa406e0317530e97d793
Victor Emmanuel <victor.j.emman@gmail.com>
2023-07-12 01:00:23 +0100

    Returning functions

---
+++ returning-functions.js
@@ -0,0 +1,13 @@
+/**
+ * In web development, declaring var at the global scope is a bad idea
+ *
+ *
+ */
+
+//don't do this
+//var myVariable = 'I am the global scope';
+
+//Don't do this;
+// var myFunction = function () {
+//     console.log('me too');
+// }
